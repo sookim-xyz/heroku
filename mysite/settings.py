845bbabaed50f5487931abcdb5d934926ae337c2
Sookyung Kim <sookyung.kim@datadoghq.com>
2021-10-26 16:55:00 +0900

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+3gk5egpo31iv9jkbgk!rf5y@j!nzs*k$10dknpk3)ick3j__x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
